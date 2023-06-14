eb90b32dc75598407f4838c67b0b3145676b8b79
Abbas Atwi <abbas_9589_new@hotmail.com>
2023-06-14 13:58:10 -0300

    update

--- assets/js/app-post.js
+++ assets/js/app-post.js
@@ -3,8 +3,7 @@ const inputsValue = document.querySelector(".section-postar input");
 const sectionContainer = document.querySelector(".post-container");
 const date = new Date();
 const hours = [date.getHours(), date.getMinutes()];
-btnPost.addEventListener("click", (e) => {
-  e.preventDefault();
+function posts() {
   const div = document.createElement("div");
   const span = document.createElement("span");
   const comments = document.createElement("p");
@@ -14,4 +13,15 @@ btnPost.addEventListener("click", (e) => {
   comments.innerHTML = inputsValue.value;
   div.appendChild(comments);
   sectionContainer.appendChild(div);
+}
+
+inputsValue.addEventListener("keyup", () => {
+  if (inputsValue.value.length > -0 && inputsValue.value.length != null) {
+    btnPost.classList.add("active");
+    // alert();
+  }
+});
+btnPost.addEventListener("click", (e) => {
+  e.preventDefault();
+  posts();
 });
