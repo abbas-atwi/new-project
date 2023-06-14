521a9380cfc4ebce1b8b0d6608d0eae081b79eb6
Abbas Atwi <abbas_9589_new@hotmail.com>
2023-06-14 14:09:12 -0300

    update

--- assets/js/app-post.js
+++ assets/js/app-post.js
@@ -3,6 +3,7 @@ const inputsValue = document.querySelector(".section-postar input");
 const sectionContainer = document.querySelector(".post-container");
 const date = new Date();
 const hours = [date.getHours(), date.getMinutes()];
+const com = inputsValue.value;
 function posts() {
   const div = document.createElement("div");
   const span = document.createElement("span");
@@ -14,14 +15,24 @@ function posts() {
   div.appendChild(comments);
   sectionContainer.appendChild(div);
 }
-
+let btnsActive = false;
 inputsValue.addEventListener("keyup", () => {
-  if (inputsValue.value.length > -0 && inputsValue.value.length != null) {
+  if (inputsValue.value.length > -0) {
+    btnsActive = true;
     btnPost.classList.add("active");
-    // alert();
+  } else {
+    btnsActive = false;
+    btnPost.classList.remove("active");
   }
 });
 btnPost.addEventListener("click", (e) => {
   e.preventDefault();
-  posts();
+  if (btnsActive) {
+    posts();
+    inputsValue.value = "";
+    btnsActive = false;
+    btnPost.classList.remove("active");
+  } else {
+    alert("escreva algo");
+  }
 });
